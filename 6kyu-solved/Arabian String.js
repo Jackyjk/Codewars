b2a08502b33a0c0dee781d40a336f63d285d9430
Jackyjk <2538808265@qq.com>
2020-09-08 22:17:19 +0800

    Arabian String.js

--- 6kyu-solved/Arabian String.js
+++ 6kyu-solved/Arabian String.js
@@ -1,4 +1,4 @@
-const camelize = str => str.split(/[^a-z0-9]/i).map(function (v) { return v.length > 0 ? v[0].toUpperCase() + v.substring(1).toLowerCase() : ''; }).join('');
+const camelize = str => str.split(/[^a-z0-9]/i).map(function (v) { return v.length > 0 ? v[0].toUpperCase() + v.substring(1).toLowerCase() : '' }).join('')
 
-console.log(camelize('hello world'));
-console.log(camelize('hello-world'));
+console.log(camelize('hello world'))
+console.log(camelize('hello-world'))
